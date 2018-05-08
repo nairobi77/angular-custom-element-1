f4dbdde039ec0cf7e30e21df43583b696d0eab47
nairobi77 <sobakko@gmail.com>
2018-05-08 15:00:00 +0300

    try fix remote js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './polyfills';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import {enableProdMode} from '@angular/core';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
